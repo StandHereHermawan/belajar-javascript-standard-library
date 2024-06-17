a6526be4ed6124f6daf8f4b5fa51e0ca123f0f99
Arief-Karditya <nolifegansaya@gmail.com>
2024-06-17 16:04:58 +0700

    Boolean, JS StdLib

---
+++ 9-boolean/boolean.js
@@ -0,0 +1,21 @@
+/**
+ * Boolean
+ */
+{
+    const bool = Boolean(true);
+
+    console.info(bool);
+    console.info(typeof bool);
+    console.info(bool.toString());
+    console.info(bool.valueOf());
+}
+
+// Menggunakan kata kunci New
+{
+    const bool = new Boolean(true);
+
+    console.info(bool);
+    console.info(typeof bool);
+    console.info(bool.toString());
+    console.info(bool.valueOf());
+}
