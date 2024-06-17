c494cc957b69fcd846a6141dd59e23ff4b2ac3ff
Arief-Karditya <nolifegansaya@gmail.com>
2024-06-17 17:05:20 +0700

    Set, JS StdLib

---
+++ 11-set/set.js
@@ -0,0 +1,11 @@
+/**
+ * Set
+ */
+const set = new Set();
+set.add("Arief");
+set.add("Arief");
+set.add("Karditya");
+set.add("Hermawan");
+
+console.info(set);
+set.forEach((value) => console.info(value));
